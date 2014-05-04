d3f80086358ded46cf5f4941a22c36246359aaf0
carlos guzman <guzmca@gmail.com>
2014-05-04 14:05:25 -0400

    Find course function added to schedule controller

--- controllers/user.js
+++ controllers/user.js
@@ -379,25 +379,3 @@ exports.postForgot = function(req, res, next) {
     res.redirect('/forgot');
   });
 };
-
-exports.findAsync = function(req, res) {
-   var regex = new RegExp(req.query["term"], 'i');
-
-   //Find exact data about classification, number and section
-   var query = User.find({course_name: regex},{ description:regex}, {instructors:regex}, {classification: regex }).sort({"updated_at":-1}).sort({"created_at":-1}).limit(10);
-      
-    // Execute query in a callback and return users list
-  query.exec(function(err, users) {
-    if (!err) {
-       // Method to construct the json result set
-       var result = buildResultSet(users); 
-       res.send(result, {
-          'Content-Type': 'application/json'
-       }, 200);
-    } else {
-       res.send(JSON.stringify(err), {
-          'Content-Type': 'application/json'
-       }, 404);
-    }
-  });
-};
